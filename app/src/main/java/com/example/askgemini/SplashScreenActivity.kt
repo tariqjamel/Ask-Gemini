c370cecdfa506590d7f8d58d08dcef0a29e3eff6
tariqjamel <tariqjamel98@gmail.com>
2024-05-08 00:56:06 +0500

    fourth commit

---
+++ app/src/main/java/com/example/askgemini/SplashScreenActivity.kt
@@ -0,0 +1,31 @@
+package com.example.askgemini
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val tvSplashScreen = findViewById<ImageView>(R.id.tvSplashScreen)
+        tvSplashScreen.alpha = 0f
+        tvSplashScreen.animate().setDuration(1000).alpha(1f).withEndAction{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.slide_out_right)
+            finish()
+        }
+    }
+}
